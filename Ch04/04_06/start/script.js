2eca5b50e3fee7865b782709df26187b0d5d37fd
Chao216 <co.jiaan@proton.me>
2022-05-09 17:32:11 +0300

    class extension

--- Ch04/04_06/start/script.js
+++ Ch04/04_06/start/script.js
@@ -15,3 +15,18 @@ let coolSkiVan = new Vehicle("cool ski van", 4);
 
 console.log(coolSkiVan);
 coolSkiVan.describeYourself();
+
+class bus extends Vehicle{
+  constructor(description, wheels, seats, doors, describeYourself){
+    super(description, wheels, describeYourself)
+    this.seats=seats;
+    this.doors=doors
+  }
+}
+
+let markov = new bus("BYD", 12, 36,3)
+
+console.log(markov)
+markov.describeYourself();
+
+// while extending a class, put old params in super(). this.newparam = newparam
