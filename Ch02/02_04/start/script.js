2cf01d1c9fb3d7e1446a613fd03e3c61aef12700
Chao216 <co.jiaan@proton.me>
2022-05-09 14:19:42 +0300

    searching strings

---
+++ Ch02/02_04/start/script.js
@@ -0,0 +1,8 @@
+const poem = 'this is a poem made by noman'
+
+console.log(poem)
+
+console.log(poem.startsWith("thi"))
+console.log(poem.endsWith("man"))
+console.log(poem.includes("")) // white space is also a type of string
+console.log(poem.search("mad"))
