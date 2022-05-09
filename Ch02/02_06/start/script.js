416f437fa2eee3b02d1fee69bf1a3af39b66b0af
Chao216 <co.jiaan@proton.me>
2022-05-09 15:26:12 +0300

    map() key value pair

---
+++ Ch02/02_06/start/script.js
@@ -0,0 +1,10 @@
+let phone = new Map()
+phone.set("xiaomi", 3299)
+phone.set("oneplus", 3999)
+phone.set("apple", 5299)
+
+console.log(phone)
+console.log(phone.get("oneplus"))
+
+// map() is collections of key& value pair.
+
