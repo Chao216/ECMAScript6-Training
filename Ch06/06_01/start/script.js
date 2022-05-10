753c471468a5c0465c4f68586db38e5d55b0567c
Chao216 <co.jiaan@proton.me>
2022-05-10 15:10:43 +0300

    promise

--- Ch06/06_01/start/script.js
+++ Ch06/06_01/start/script.js
@@ -1,5 +1,36 @@
-const yanwu = (seconds)=>
-new Promise((resolve)=>
-setTimeout(resolve, seconds*1000))
+// const yanwu = (seconds)=>
+// new Promise((resolve)=>
+// setTimeout(resolve, seconds*1000))
 
-yanwu(13).then(()=> console.log("13 secs 延误了"))
+// yanwu(13).then(()=> console.log("13 secs 延误了"))
+
+// const delay = (seconds) => {
+//     new Promise((resolve, reject)=>{
+//         if (seconds >= 30) {
+//             reject(
+//                 new Error("please insert less than 30 seconds")
+//                 );
+//         }
+
+//         setTimeout(resolve, seconds*1000);
+//     });
+    
+// };
+
+// delay(20).then(()=>console.log("delayed response"))
+
+const beat = (value1)=>{
+    new Promise((right, wrong)=>{
+        if (value1 > 10) {
+            wrong (
+                new Error("the number your put is big!!!")
+            );
+        }
+        else {
+            right (console.log("the time you select is ", value1))
+        }
+        
+    })
+}
+
+beat(110)
